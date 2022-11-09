4d861660c1e6da2f0cd9e901d5b0c8340c291b51
Richard <richard.staszkiewicz@gmail.com>
2022-11-09 11:50:21 +0100

    [ASTAR] Basic test for creation of Astar class basing on Graph

--- include/Astar.hpp
+++ include/Astar.hpp
@@ -12,6 +12,9 @@
 #include <string>
 #include <iostream>
 
-class Astar {};
+class Astar
+{
+
+};
 
 #endif
--- test/test_astar.cpp
+++ test/test_astar.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
 #include <../include/Astar.hpp>
+#include <../include/Graph.hpp>
 #include <vector>
 #include <string>
 
 TEST(sanity_test, sanity_test)
 {
     EXPECT_EQ(true, true);
+}
+
+TEST(construction_tests, graph_import)
+{
+    Graph g(std::string("../test/test_data/test1"), false);
+    Astar alg1(g);
 }
 
 int main(int argc, char **argv)
